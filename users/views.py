6c4bb961bd3984051580b5d6b148f3ecbed9d3be
apple <apple@apples-MacBook-Pro.local>
2022-07-14 23:30:01 +0900

    - EDIT : signin and bcrypt and jwt

--- users/views.py
+++ users/views.py
@@ -1,8 +1,11 @@
 import json
 import re
+import bcrypt
+import jwt
 
 from django.views import View
 from django.http import JsonResponse
+from django.conf import settings
 
 from .models import User
 
@@ -20,10 +23,12 @@ class SignUpView(View):
             check_password(password)
             check_phone_number(phone_number)
 
+            hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
+
             User.objects.create(
                 name            = name,
                 email           = email,
-                password        = password,
+                password        = hashed_password,
                 phone_number    = phone_number
             )
 
@@ -33,6 +38,30 @@ class SignUpView(View):
             return JsonResponse({"message":"키에러"}, status=400)
         except ValueError as e:
             return JsonResponse({"message":f"{e}"}, status=400)
+
+class SignInView(View):
+    def post(self,request):
+        try:
+            data        = json.loads(request.body)
+            email       = data["email"]
+            password    = data["password"]
+            user        = User.objects.get(email=email)
+
+            if not bcrypt.checkpw(password.encode("utf-8"), user.password.encode("utf-8")):
+                return JsonResponse({"message":"잘못된 비밀번호 입니다."}, status=400)
+
+            access_token = jwt.encode({"id":user.id}, settings.SECRET_KEY, settings.ALGORITHM)
+            return JsonResponse(
+                {"message":"로그인에 성공하였습니다.",
+                "access_token": access_token},
+                status=200
+            )
+
+        except User.DoesNotExist:
+            return JsonResponse({"message":"존재하지 않는 이메일입니다."}, status=400)
+        except KeyError:
+            return JsonResponse({"message" : "키에러"}, status=400)
+
 
 def check_email(email):
     regx_email = '^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
